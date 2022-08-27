62414a8694a9302f723c6c4ae5d1502f140f0ea9
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2022-08-27 14:07:20 +0200

    Add DatePicker and HourPicker classes

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,7 @@
 import { templates, select } from '../settings.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 class Booking {
   constructor(element) {
@@ -19,6 +21,12 @@ class Booking {
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(
       select.booking.hoursAmount
     );
+    thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(
+      select.widgets.hourPicker.wrapper
+    );
+    thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(
+      select.widgets.datePicker.wrapper
+    );
   }
   initWidgets() {
     const thisBooking = this;
@@ -26,6 +34,8 @@ class Booking {
     thisBooking.dom.hoursAmount.addEventListener('updated', function () {});
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.dom.peopleAmount.addEventListener('updated', function () {});
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
   }
 }
 
--- src/js/components/DatePicker.js
+++ src/js/components/DatePicker.js
@@ -1,49 +1,52 @@
 import BaseWidget from '../components/BaseWidget.js';
 import utils from '../utils.js';
-import {select, settings} from '../settings.js';
+import { select, settings } from '../settings.js';
 
-class DatePicker extends BaseWidget{
-  constructor(wrapper){
+class DatePicker extends BaseWidget {
+  constructor(wrapper) {
     super(wrapper, utils.dateToStr(new Date()));
     const thisWidget = this;
 
-    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.datePicker.input);
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(
+      select.widgets.datePicker.input
+    );
     thisWidget.initPlugin();
   }
-  initPlugin(){
+  initPlugin() {
     const thisWidget = this;
 
     thisWidget.minDate = new Date();
-    thisWidget.maxDate = utils.addDays(thisWidget.minDate, settings.datePicker.maxDaysInFuture);
+    thisWidget.maxDate = utils.addDays(
+      thisWidget.minDate,
+      settings.datePicker.maxDaysInFuture
+    );
     // eslint-disable-next-line no-undef
     flatpickr(thisWidget.dom.input, {
       defaultDate: thisWidget.minDate,
       minDate: thisWidget.minDate,
       maxDate: thisWidget.maxDate,
       locale: {
-        firstDayOfWeek: 1
+        firstDayOfWeek: 1,
       },
       disable: [
-        function(date) {
-          return (date.getDay() === 1);
-        }
+        function (date) {
+          return date.getDay() === 1;
+        },
       ],
-      onChange: function(selectedDates, dateStr) {
+      onChange: function (selectedDates, dateStr) {
         thisWidget.value = dateStr;
       },
     });
   }
-  parseValue(value){
+  parseValue(value) {
     return value;
   }
 
-  isValid(){
+  isValid() {
     return true;
   }
 
-  renderValue(){
-
-  }
+  renderValue() {}
 }
 
 export default DatePicker;
